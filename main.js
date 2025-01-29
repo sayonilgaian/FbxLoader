f355dea7883519fc52f65eab0fb1b6ce4b941f4d
sayonil-mitra <sayonil.m@mobiusdtaas.ai>
2025-01-29 12:15:07 +0530

    Added environment

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { fbxLoader, progressiveFBXLoader } from './src/utils/fbxLoader';
-import {gLTFLoader, progressiveGLTFLoader} from './src/utils/gltfLoader';
+import { gLTFLoader, progressiveGLTFLoader } from './src/utils/gltfLoader';
 import addTestCube from './src/utils/addTestCube';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -20,11 +21,19 @@ document.body.appendChild(renderer.domElement);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(1000, 1000, 0);
-directionalLight.castShadow = true;
-directionalLight.shadow.bias = -0.005; // Fine-tune this value
-scene.add(directionalLight);
+const directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight1.position.set(0, 1000, 0);
+directionalLight1.castShadow = true;
+directionalLight1.shadow.bias = -0.005; // Fine-tune this value
+scene.add(directionalLight1);
+
+// Add environment
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+scene.background = new THREE.Color(0xbfe3dd);
+scene.environment = pmremGenerator.fromScene(
+	new RoomEnvironment(),
+	0.04
+).texture;
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -34,7 +43,11 @@ controls.update();
 
 // Pass the scene to the loader
 // fbxLoader({ fileName: './src/models/LHR.fbx', scene });
-progressiveFBXLoader({ fileName: './src/models/LHR.fbx', scene, rotateX:-Math.PI / 2 });
+progressiveFBXLoader({
+	fileName: './src/models/LHR.fbx',
+	scene,
+	rotateX: -Math.PI / 2,
+});
 // gLTFLoader({ fileName: './src/models/LHR.glb', scene });
 // progressiveGLTFLoader({ fileName: './src/models/LHR.glb', scene });
 // addTestCube({ scene });
